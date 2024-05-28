e976df8c6b553b3f16ffef652736e6b8ef662e2c
YiBangRen <overlord0410@163.com>
2024-05-28 09:51:26 +0800

    ADD：鼠标选中下颚，并移动

---
+++ Assets/Script/JawMover.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JawMover : MonoBehaviour
+{
+    Vector2 screenMousePosition = Vector2.zero;
+    Vector3 worldMousePositon = Vector3.zero;
+    LayerMask lowerJawLayer;
+    Vector3 dragDisplacement;
+    [HideInInspector] public Collider2D targetCollider;
+    [SerializeField] private GameObject allLowerJaw;
+    private bool isMouseBtnLeftDown = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       lowerJawLayer = LayerMask.GetMask("LowerJaw");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            screenMousePosition = Input.mousePosition;
+            worldMousePositon = Camera.main.ScreenToWorldPoint(screenMousePosition);
+            Vector2 worldPos2D = new Vector2(worldMousePositon.x, worldMousePositon.y);
+            targetCollider = Physics2D.OverlapPoint(worldPos2D, lowerJawLayer);
+            if (targetCollider != null)
+            {
+
+                if (!isMouseBtnLeftDown)
+                {
+                    
+                    dragDisplacement = allLowerJaw.transform.position - worldMousePositon;
+                }
+
+                isMouseBtnLeftDown = true;
+                allLowerJaw.transform.position = worldMousePositon + dragDisplacement;  
+            }
+            else
+            {
+                isMouseBtnLeftDown = false;
+            }
+        }
+        
+    }
+}
